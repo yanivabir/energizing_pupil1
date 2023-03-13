d96aca423d31fc6ec9edee67c708d464c483626f
yanivabir <yanivabir@gmail.com>
2023-03-12 22:01:55 -0400

    ET settings

--- c0.1.py
+++ c0.1.py
@@ -221,6 +221,46 @@ except RuntimeError as err:
     core.quit()
     sys.exit()
 
+# Step 3: Configure the tracker
+#
+# Put the tracker in offline mode before we change tracking parameters
+el_tracker.setOfflineMode()
+
+# Get the software version:  1-EyeLink I, 2-EyeLink II, 3/4-EyeLink 1000,
+# 5-EyeLink 1000 Plus, 6-Portable DUO
+eyelink_ver = 0  # set version to 0, in case running in Dummy mode
+if not dummy_mode:
+    vstr = el_tracker.getTrackerVersionString()
+    eyelink_ver = int(vstr.split()[-1].split('.')[0])
+    # print out some version info in the shell
+    print('Running experiment on %s, version %d' % (vstr, eyelink_ver))
+
+# File and Link data control
+# what eye events to save in the EDF file, include everything by default
+file_event_flags = 'LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON,INPUT'
+# what eye events to make available over the link, include everything by default
+link_event_flags = 'LEFT,RIGHT,FIXATION,SACCADE,BLINK,BUTTON,FIXUPDATE,INPUT'
+# what sample data to save in the EDF data file and to make available
+# over the link, include the 'HTARGET' flag to save head target sticker
+# data for supported eye trackers
+if eyelink_ver > 3:
+    file_sample_flags = 'LEFT,RIGHT,GAZE,HREF,RAW,AREA,HTARGET,GAZERES,BUTTON,STATUS,INPUT'
+    link_sample_flags = 'LEFT,RIGHT,GAZE,GAZERES,AREA,HTARGET,STATUS,INPUT'
+else:
+    file_sample_flags = 'LEFT,RIGHT,GAZE,HREF,RAW,AREA,GAZERES,BUTTON,STATUS,INPUT'
+    link_sample_flags = 'LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,INPUT'
+el_tracker.sendCommand("file_event_filter = %s" % file_event_flags)
+el_tracker.sendCommand("file_sample_data = %s" % file_sample_flags)
+el_tracker.sendCommand("link_event_filter = %s" % link_event_flags)
+el_tracker.sendCommand("link_sample_data = %s" % link_sample_flags)
+
+# Optional tracking parameters
+# Sample rate, 250, 500, 1000, or 2000, check your tracker specification
+if eyelink_ver > 2:
+    el_tracker.sendCommand("sample_rate 1000")
+# Choose a calibration type, H3, HV3, HV5, HV13 (HV = horizontal/vertical),
+el_tracker.sendCommand("calibration_type = HV9")
+
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
